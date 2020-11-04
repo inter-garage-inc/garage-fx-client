e0cbbd3c9f172fb7dd00e69a1d7c899c7d7f4434
FelipePy <felipespsampaiomotasp@gmail.com>
2020-11-04 10:06:14 -0300

    New screen ManagerNotFound
    Além da nova tela, criei uma nova classe que será responsável por guardar os botões e atributos principais.

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -6,14 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("EmployeeHome.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("images/isotypeBlue.png")));
         primaryStage.setTitle("Login - Garage Inc.");
