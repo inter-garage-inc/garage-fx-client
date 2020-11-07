fa1ea9310e932afc7487e1f11c5de2b4495e6620
jlucasrods <jlucasrods@gmail.com>
2020-11-07 14:29:40 -0300

    Create and configure an http client

---
+++ src/main/java/app/client/GarageClient.java
@@ -0,0 +1,59 @@
+package app.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class GarageClient {
+    public final static String HOST = "http://localhost:8080/";
+
+    public static HttpResponse<?> get(String resource) throws IOException, InterruptedException {
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        var request = HttpRequest.newBuilder()
+                .GET()
+                .header("Content-Type", "application/json")
+                .uri(URI.create(HOST + resource))
+                .build();
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<?> delete(String resource) throws IOException, InterruptedException {
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        var request = HttpRequest.newBuilder()
+                .DELETE()
+                .header("Content-Type", "application/json")
+                .uri(URI.create(HOST + resource))
+                .build();
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<?> create(String resource, Object payload) throws IOException, InterruptedException {
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        var request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString((String) payload))
+                .header("Content-Type", "application/json")
+                .uri(URI.create(HOST + resource))
+                .build();
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+
+    public static HttpResponse<?> update(String resource, Object payload) throws IOException, InterruptedException {
+        var httpClient = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .build();
+        var request = HttpRequest.newBuilder()
+                .PUT(HttpRequest.BodyPublishers.ofString((String) payload))
+                .header("Content-Type", "application/json")
+                .uri(URI.create(HOST + resource))
+                .build();
+        return httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+    }
+}
---
+++ src/main/java/app/data/User.java
@@ -0,0 +1,24 @@
+package app.data;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class User {
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("role")
+    private String role;
+
+    @JsonProperty("created_at")
+    private Date createdAt;
+}
