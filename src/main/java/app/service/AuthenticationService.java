2ca5ffa128a542cd238156cc118c4be10c41196f
jlucasrods <jlucasrods@gmail.com>
2020-11-13 21:26:22 -0300

    Allows recover user from Jwt authentication

--- src/main/java/app/service/AuthenticationService.java
+++ src/main/java/app/service/AuthenticationService.java
@@ -4,6 +4,7 @@ import app.client.GarageClient;
 import app.data.Credentials;
 import app.data.Jwt;
 import app.data.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Claims;
@@ -14,48 +15,29 @@ import java.io.IOException;
 
 public class AuthenticationService {
 
-    private ObjectMapper mapper;
-
     private static Jwt jwt;
+
+    private final ObjectMapper mapper;
 
     public AuthenticationService() {
         mapper = new ObjectMapper();
     }
 
-    public Boolean login(Credentials authRequest) {
+    public Boolean login(Credentials authRequest) throws ConnectionFailureException {
         try {
             var payload = mapper.writeValueAsString(authRequest);
             var response = GarageClient.post("/authentication", payload);
             if(response.statusCode() == 200) {
-                jwt = mapper.readValue((String) response.body(), new TypeReference<Jwt>(){});
-                return Boolean.TRUE;
-            } else {
-        return Boolean.FALSE;
+                jwt = mapper.readValue((String) response.body(), new TypeReference<Jwt>() {});
+                return true;
             }
+            return false;
         } catch (IOException | InterruptedException exception) {
-            return null;
+            throw new ConnectionFailureException();
         }
     }
 
-    @SneakyThrows
-    public User claimUser() {
-        var claims = getJwtClaims();
-        var userJson = claims.get("user", String.class);
-        return mapper.readValue(userJson, User.class);
-    }
-
-    private String getJwtWithoutSignature() {
-        var i = jwt.getToken().lastIndexOf('.');
-        return jwt.getToken().substring(0, i + 1);
-    }
-
-    private Claims getJwtClaims() {
-        return Jwts.parser()
-                .parseClaimsJwt(getJwtWithoutSignature())
-                .getBody();
-    }
-
-    public static String getToken() {
-        return (jwt != null) ? jwt.getToken() : "";
+    public static Jwt getJwt() {
+        return jwt;
     }
 }
