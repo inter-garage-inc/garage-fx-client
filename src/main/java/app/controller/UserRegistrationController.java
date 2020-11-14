88ec39294d73c0e2f7010170469bcc3164250f8b
Thaís Tarora <ttarora86@gmail.com>
2020-11-14 17:10:54 -0300

    Created user registration form and improvements

---
+++ src/main/java/app/controller/UserRegistrationController.java
@@ -0,0 +1,11 @@
+package app.controller;
+
+import app.router.RouteMapping;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+@RouteMapping(title = "Cadastro de Usuário")
+public class UserRegistrationController extends ApplicationController {
+
+}
