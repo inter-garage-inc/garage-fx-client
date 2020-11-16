f7feea6ce5ba72eea1f70bfb5ffff5979ada7d7e
jlucasrods <jlucasrods@gmail.com>
2020-11-16 17:04:09 -0300

    Add date and time to home

--- src/main/java/app/controller/HomeController.java
+++ src/main/java/app/controller/HomeController.java
@@ -1,11 +1,18 @@
 package app.controller;
 
 
-import app.controller.component.MainMenuController;
 import app.router.RouteMapping;
 import app.service.AuthenticationService;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @RouteMapping(title = "Página inicial")
 public class HomeController {
@@ -14,9 +21,24 @@ public class HomeController {
     private Label labelWelcomeMessage;
 
     @FXML
-    MainMenuController mainMenuController;
+    private Label labelLocalDateTime;
 
     public void initialize() {
+        initClock();
+        initWelcome();
+    }
+
+    private void initClock() {
+        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            var locale = new Locale("pt", "BR");
+            var formatter = DateTimeFormatter.ofPattern("EEEE, dd 'de' MMMM 'de' yyyy \n HH:mm:ss").localizedBy(locale);
+            labelLocalDateTime.setText(LocalDateTime.now().format(formatter));
+        }), new KeyFrame(Duration.seconds(1)));
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    private void initWelcome() {
         var user = AuthenticationService.claimUser();
         labelWelcomeMessage.setText("Bem-vindo " + user.getName());
     }
