29d454aa7e4e1756dd834751500d5b0b3db8908f
José Lucas <38442731+jlucasrods@users.noreply.github.com>
2020-11-18 08:38:30 -0300

    Revert "Create customer registration routine"

--- src/main/java/app/data/address/Country.java
+++ src/main/java/app/data/address/Country.java
@@ -1,34 +1,16 @@
 package app.data.address;
 
-import app.data.address.state.Brazil;
-
 public enum Country {
-    BRAZIL("Brasil", Brazil.values()),
-    OTHER("Outro");
+    BRAZIL("Brazil"),
+    OTHER("other");
 
     private final String value;
-    private final State[] states;
 
     Country(final String value) {
         this.value = value;
-        this.states = null;
-    }
-
-    Country(final String value, State[] states) {
-        this.value = value;
-        this.states = states;
     }
 
     public String getValue() {
         return value;
-    }
-
-    public State[] getStates() {
-        return states;
-    }
-
-    @Override
-    public String toString() {
-        return this.getValue();
     }
 }
