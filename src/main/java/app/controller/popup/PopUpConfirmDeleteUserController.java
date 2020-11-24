ef7e3d58684f901435115cfaeab382f88c2865cf
FelipePy <felipespsampaiomotasp@gmail.com>
2020-11-24 13:06:09 -0300

    pin

--- src/main/java/app/controller/popup/PopUpConfirmDeleteUserController.java
+++ src/main/java/app/controller/popup/PopUpConfirmDeleteUserController.java
@@ -1,20 +1,18 @@
 package app.controller.popup;
-
 import app.controller.AlterDeletUserController;
 import app.router.RouteMapping;
 import app.router.Router;
 import javafx.scene.control.Button;
-
 @RouteMapping(title = "Confirmação de exclusão", popup = true)
 public class PopUpConfirmDeleteUserController {
     public Button btnDelete;
     public Button btnClose;
 
-    public void handleOnActionButtonBtnDelete() {
-        Router.
+    public Integer handleOnActionButtonBtnDelete() {
+        return 1;
     }
 
-    public void handleOnActionButtonBtnClose() {
-        Router.goTo(AlterDeletUserController.class, 0);
+    public Integer handleOnActionButtonBtnClose() {
+        return 1;
     }
 }
