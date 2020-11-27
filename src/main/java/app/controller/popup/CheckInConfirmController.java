8d2b68c4c595e514d45514773af31d015c382acb
FelipePy <felipespsampaiomotasp@gmail.com>
2020-11-27 20:00:09 -0300

    pin

---
+++ src/main/java/app/controller/popup/CheckInConfirmController.java
@@ -0,0 +1,7 @@
+package app.controller.popup;
+
+import app.router.RouteMapping;
+
+@RouteMapping(title = "Checkin", popup = true)
+public class CheckInConfirmController {
+}
---
+++ src/main/java/app/data/catalog/CatalogType.java
@@ -0,0 +1,22 @@
+package app.data.catalog;
+
+public enum CatalogType {
+
+    PARKING("estacionamento"),
+    OTHER("outros");
+
+    private final String value;
+
+    CatalogType(final String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return getValue();
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
