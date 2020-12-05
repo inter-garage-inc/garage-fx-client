516eb1c0d78614e6c8502dfe5a26ae94541bd8f6
jlucasrods <jlucasrods@gmail.com>
2020-12-05 13:07:09 -0300

    Add javadoc for the missing services

--- src/main/java/app/service/ItemsService.java
+++ src/main/java/app/service/ItemsService.java
@@ -8,13 +8,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+/**
+ * This class is responsible for the Garage Inc. Items transactions
+ * Can get items, save a new one, update it or delete it.
+ *
+ * @author FelipePy
+ * @version 1.0
+ * @since 2020-11-17
+ */
 public class ItemsService {
+
+    /**
+     * Helper tool to serialize and deserialize POJO and Json to each other
+     */
     private ObjectMapper mapper;
 
     public ItemsService() {
         mapper = new ObjectMapper();
     }
 
+    /**
+     * This method is used to save a new Item
+     *
+     * @param item POJO
+     * @return true if is successfully saved of false otherwise
+     * @throws ConnectionFailureException when request to the server fails
+     */
     public Boolean save(Item item) throws ConnectionFailureException {
         try {
             var payload = mapper.writeValueAsString(item);
--- src/main/java/app/service/VehiclesService.java
+++ src/main/java/app/service/VehiclesService.java
@@ -9,13 +9,27 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
+/**
+ * This class is responsible for the Garage Inc. vehicles transactions
+ */
 public class VehiclesService {
+
+    /**
+     * Helper tool to serialize and deserialize POJO and Json to each other
+     */
     private ObjectMapper mapper;
 
     public VehiclesService() {
         mapper = new ObjectMapper();
     }
 
+    /**
+     * This method is used to get an existing vehicle by its license plate
+     *
+     * @param licensePlate Vehicle license plate
+     * @return a Vehicle POJO if one is successfully found or false otherwise
+     * @throws ConnectionFailureException when request to the server fails
+     */
     public Vehicle findByLicensePlate(String licensePlate) throws ConnectionFailureException {
         try {
             var response = GarageClient.get("/vehicles/license-plate/" + licensePlate);
@@ -27,6 +41,13 @@ public class VehiclesService {
         }
     }
 
+    /**
+     * This method is used to save a new vehicle
+     *
+     * @param vehicle POJO
+     * @return a Vehicle POJO if is successfully saved or false otherwise
+     * @throws ConnectionFailureException when request to the server fails
+     */
     public Vehicle save(Vehicle vehicle) throws ConnectionFailureException {
         try {
             var payload = mapper.writeValueAsString(vehicle);
@@ -39,6 +60,13 @@ public class VehiclesService {
         }
     }
 
+    /**
+     * This method is used to delete a vehicle
+     *
+     * @param id Vehicle id
+     * @return true if is successfully deleted or false otherwise
+     * @throws ConnectionFailureException when request to the server fails
+     */
     public Boolean delete(Long id) throws ConnectionFailureException {
         try {
             var response = GarageClient.delete("/vehicles/" + id);
